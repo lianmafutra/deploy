a99033b2f545d27defa88517d2f9cf0c4afe9f2d
Lian Mafutra <lianmafutra@gmail.com>
2023-05-28 17:04:25 +0700

    deploy update

--- config/deploy.php
+++ config/deploy.php
@@ -2,18 +2,23 @@
 /*
      * Server Configuration
      *'host' => "103.20.xx.xx",
-      'port' => "22", 
+      'port' => "22", ( default )
       'path' => "www/project-name/",
       'username' => "root-user",
       'password' => "root-pass",
      */
 return [
    'server' => [
-      'host' => "",
-      'port' => "22",
-      'path' => "",
+      'host'     => "",
+      'port'     => "22",
+      'path'     => "",
       'username' => "",
       'password' => "",
+   ],
+   'git-ftp'=>[
+      "url"       => "",
+      "user"      => "",
+      "password " => "",
    ],
    'deploy' => [
       "password" => ""
