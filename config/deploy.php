8327eacfcc3a57608e329827f14e764794cae216
Lian Mafutra <lianmafutra@gmail.com>
2023-06-02 08:53:44 +0700

    update deploy config format

--- config/deploy.php
+++ config/deploy.php
@@ -1,23 +1,23 @@
 <?php
 return [
    'server'=>[
-      'host'     => env("DEPLOY_HOST"),
-      'port'     => env("DEPLOY_PORT"),
-      'path'     => env("DEPLOY_PATH"),
-      'username' => env("DEPLOY_USER"),
-      'password' => env("DEPLOY_PASS"),
+      'host'     => env('DEPLOY_HOST'),
+      'port'     => env('DEPLOY_PORT'),
+      'path'     => env('DEPLOY_PATH'),
+      'username' => env('DEPLOY_USER'),
+      'password' => env('DEPLOY_PASS'),
    ],
    'git-ftp'=>[
-      "url"      => env("FTP_URL"),
-      "user"     => env("FTP_USER"),
-      "password" => env("FTP_PASS"),
+      'url'      => env('FTP_URL'),
+      'user'     => env('FTP_USER'),
+      'password' => env('FTP_PASS'),
    ],
    'deploy'=>[
-      "route_cache"      => true,
-      "config_cache"     => true,
-      "view_cache"       => true,
-      "maintenance_mode" => true,
-      "password"     => ""
+      'route_cache'      => true,
+      'config_cache'     => true,
+      'view_cache'       => true,
+      'maintenance_mode' => true,
+      'password'         => ''
    ]
 ];
 
